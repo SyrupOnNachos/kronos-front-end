4a79c9d91772b41c8a553ac4a5ec452abd1e9094
Austin <80305855+SyrupOnNachos@users.noreply.github.com>
2024-01-28 18:44:57 -0600

    Added ability to create account

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -7,17 +7,17 @@ import {
   TextField,
   Stack,
 } from "@mui/material";
-import { useLoginMutation } from "../api";
+import { useCreateLoginMutation } from "../api";
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const navigate = useNavigate();
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [login] = useLoginMutation();
+const CreateAccount = () => {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [createLogin] = useCreateLoginMutation();
 
-  return (
-    <Box
+    return (
+        <Box
       sx={{
         display: "flex",
         justifyContent: "center",
@@ -34,7 +34,7 @@ const Login = () => {
         {/* Horizontal Line seperating the text from the login */}
         <hr />
 
-        <Typography variant="h4">Login</Typography>
+        <Typography variant="h4">Create Account</Typography>
         <Box component="form" noValidate autoComplete="off" sx={{ mt: 2 }}>
           <TextField
             label="Username"
@@ -54,37 +54,29 @@ const Login = () => {
           <Box sx={{ textAlign: "center" }}>
             <Stack direction="row" spacing={2}>
               <Button
-                variant="outlined"
-                fullWidth
-                onClick={() => navigate("/create-account")}
-              >
-                Create Account
-              </Button>
-              <Button
                 variant="contained"
                 color="primary"
-                // TODO: fix type="submit" so that it waits for token before refresh
-                //   type="submit"
                 fullWidth
                 onClick={() => {
-                  login({ username: username, password: password })
+                  createLogin({ username: username, password: password })
                     .unwrap()
                     .then((res) => {
                       localStorage.setItem("token", res.token),
-                        navigate("/home");
+                        navigate("/login");
                     })
                     // TODO: Add snackbar/ incorrect password management
                     .catch((err) => console.log(err));
                 }}
               >
-                Submit
+                Create
               </Button>
             </Stack>
           </Box>
         </Box>
       </Paper>
     </Box>
-  );
-};
+    )
 
-export default Login;
+    }
+
+export default CreateAccount;
